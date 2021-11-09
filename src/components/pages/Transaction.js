9bc452c0fa5a507b3c92f0515e56d42220e9a565
godwin olele <godwinolele10@gmail.com>
2021-11-09 21:28:09 +0000

    added box shadow

--- src/components/pages/Transaction.js
+++ src/components/pages/Transaction.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 
 const Transaction = () => {
     return (
@@ -8,7 +8,15 @@ const Transaction = () => {
                 <div className="base1"></div>
                 <div className="base2">
                     <div className="phone-ui">
-                        <div className="fund-balance"></div>
+                        <div className="fund-balance">
+                            <p className="ab">Account Balance</p>
+                            <p className="amount">$50,000</p>
+                            <p className="acc-num">Account no: <span className="acc-number">6156738012</span></p>
+                            <div className="buttons">
+                            <Link className="add-money">Add money</Link>
+                            <Link className="send-money">Send money</Link>
+                            </div>
+                        </div>
                         <div className="quick-action"></div>
                         <div className="transaction-history"></div>
                     </div>
